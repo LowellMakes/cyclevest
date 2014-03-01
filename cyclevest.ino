52bdb93fa7874977a19d8ad6553e46e35c150373
leftyfb <leftyfb@left-click.org>
2014-03-01 15:54:33 -0500

    modified for display at ATHM

--- cyclevest.ino
+++ cyclevest.ino
@@ -5,53 +5,71 @@ unsigned char rightbuttonState = 0;
 volatile unsigned int counter =0;
 
 void setup() {                
-  pinMode(2, OUTPUT);  // channel A  
-  pinMode(3, OUTPUT);  // channel B   
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
+  pinMode(5, OUTPUT);  
+  pinMode(6, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
   pinMode(A2, INPUT);  
   pinMode(A3, INPUT);
 }
 
 void loop(){
-  leftbuttonState = digitalRead(leftSignal);
-  rightbuttonState = digitalRead(rightSignal);
-  if (leftbuttonState == LOW) 
-  {
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    while(counter <= 10)
-    {
-       counter ++;
-       digitalWrite(2, HIGH);
-       delay(100); 
-       digitalWrite(2, LOW);
-       delay(100); 
-    }
-    counter =0;  
-  }
+//  leftbuttonState = digitalRead(leftSignal);
+//  rightbuttonState = digitalRead(rightSignal);
+//  if (leftbuttonState == LOW) 
+//  {
+//    digitalWrite(4, LOW);
+//    digitalWrite(5, LOW);
+//    while(counter <= 10)
+//    {
+//       counter ++;
+//       digitalWrite(2, HIGH);
+//       delay(100); 
+//       digitalWrite(2, LOW);
+//       delay(100); 
+//    }
+//    counter =0;  
+//  }
   
-  if (rightbuttonState == LOW) 
-  {
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    while(counter <= 15)
-    {
-      counter ++;
-      digitalWrite(3, HIGH);
-      delay(100); 
-      digitalWrite(3, LOW);
-      delay(100);
-    }
-    counter =0;
-  }
+//  if (rightbuttonState == LOW) 
+//  {
+//    while(counter <= 15)
+//    {
+//      counter ++;
+//      digitalWrite(5, HIGH);
+//      delay(500); 
+//      digitalWrite(6, LOW);
+//      delay(500);
+//    }
+//    counter =0;
+//  }
   
-  if((rightbuttonState ==HIGH)&&(leftbuttonState ==HIGH))
-  {
-  //  digitalWrite(2, LOW);
-  //  digitalWrite(3, LOW);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);   
-  }
+//  if((rightbuttonState ==HIGH)&&(leftbuttonState ==HIGH))
+//  {
+//    digitalWrite(9, HIGH);
+//    delay(500);
+//    digitalWrite(10, HIGH);
+//    delay(500);   
+//  }
+   for (int i=0; i <= 5; i++){  
+   digitalWrite(5,HIGH);
+   digitalWrite(6,LOW);
+   delay(500);
+   digitalWrite(5,LOW);
+   digitalWrite(6,HIGH);
+   delay(500);
+   }
+   digitalWrite(5,LOW);
+   digitalWrite(6,LOW);
 
+   for (int i=0; i <= 5; i++){  
+   digitalWrite(10,HIGH);
+   digitalWrite(11,LOW);
+   delay(500);
+   digitalWrite(10,LOW);
+   digitalWrite(11,HIGH);
+   delay(500);
+   }
+   digitalWrite(10,LOW);
+   digitalWrite(11,LOW);
 }
